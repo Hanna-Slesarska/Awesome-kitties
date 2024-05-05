0a32d10503693c11bc12b5148c087a3d0143d6c2
Hanna-Slesarska <slesarskaya.a@gmail.com>
2024-05-05 10:04:42 -0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+export function webpack(cfg) {
+    cfg.module.rules.push(
+        {
+            test: /\.md$/,
+            loader: 'frontmatter-markdown-loader',
+            options: { mode: ['react-component'] }
+        }
+    )
+    return cfg
+}
